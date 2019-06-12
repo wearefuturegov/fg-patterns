3d1488e36ac33974fa334c797da2f02e91044231
userman123 <dominic.freeman123@gmail.com>
2019-06-12 16:54:43 +0100

    Factory bot config

---
+++ spec/factories.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+
+  factory :pattern do
+    name { "Check something" }
+    description { "Lorem ipsum dolor amet fanny pack cray vice, single-origin coffee vape raw denim skateboard marfa meditation sriracha locavore drinking vinegar brooklyn meggings. Taiyaki edison bulb snackwave iPhone vice, echo park tacos selvage polaroid offal. Echo park truffaut next level, skateboard freegan coloring book vinyl. Selvage street art gochujang stumptown celiac hot chicken pok pok waistcoat affogato 90's cardigan mixtape." }
+  end
+end
