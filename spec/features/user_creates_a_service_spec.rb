5e37473dac0d6fd10276542792157393b2ab4a91
userman123 <dominic.freeman123@gmail.com>
2019-06-20 15:04:21 +0100

    Get feature specs working (capybara db cleaner) and look at org types

--- spec/features/user_creates_a_service_spec.rb
+++ spec/features/user_creates_a_service_spec.rb
@@ -2,15 +2,25 @@ require "rails_helper"
 
 RSpec.feature "Service creation", :type => :feature do
 
+  before(:all) do
+    Rails.application.load_seed
+  end
+
+  after(:all) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
   scenario "User creates a new service" do
     visit new_service_path
 
     fill_in 'Name', with: 'Name of a new service'
 
-    #page.find(:css, "service_patterns_1")
+    select 'County council', from: 'Organisation type'
     click_button 'Create service'
 
     expect(page).to have_text('Thankyou!')
+    expect(Service.last.name).to eq('Name of a new service')
+    expect(Service.last.organisation_type).to eq('County council')
   end
 
 end
