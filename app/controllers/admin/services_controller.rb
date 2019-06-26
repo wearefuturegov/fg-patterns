ec1596ad39f5ecc03623bc348792829da4626f19
userman123 <dominic.freeman123@gmail.com>
2019-06-26 13:18:57 +0100

    Move form into partial and allow for publishing of services on moderation

--- app/controllers/admin/services_controller.rb
+++ app/controllers/admin/services_controller.rb
@@ -6,4 +6,20 @@ class Admin::ServicesController < Admin::BaseController
   def edit
     @service = Service.find(params[:id])
   end
+
+  def update
+    @service = Service.find(params[:id])
+    if @service.update_attributes(service_params.merge(status: 'published'))
+      flash[:notice] = "Service #{@service.name} was published"
+      redirect_to patterns_path
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def service_params
+    params.require(:service).permit(:name, :steps, :suggester_email, :suggester_organisation, :transactional, organisation_types: [], pattern_ids: [], life_event_ids: [])
+  end
 end
--- spec/features/admin_moderates_a_service_spec.rb
+++ spec/features/admin_moderates_a_service_spec.rb
@@ -12,8 +12,10 @@ RSpec.feature "Service moderation", :type => :feature do
 
   scenario "Admin approves a new service" do
     service = Service.create(FactoryBot.attributes_for(:service))
+
     visit admin_services_path
     expect(page).to_not have_text('Sevices awaiting approval')
+
     page.driver.browser.authorize(ENV['HTTP_USERNAME'], ENV['HTTP_PASSWORD'])
     visit admin_services_path
     expect(page).to have_text('Services awaiting approval')
@@ -21,7 +23,14 @@ RSpec.feature "Service moderation", :type => :feature do
     expect(page).to have_text(service.suggester_organisation)
     expect(page).to have_text(service.suggester_name)
     expect(page).to have_text(service.created_at)
+
     click_link 'Moderate service'
     expect(page).to have_text("Edit #{service.name}")
+    fill_in 'Name', with: 'Name of new service edited'
+
+    click_button 'Publish this service'
+    service.reload
+    expect(page).to have_text("Service #{service.name} was published")
+    expect(page.find('.pattern_table')).to have_text(service.name)
   end
 end
