6d9d7d3b78dcd4d4c835429c52c3ec41178005b3
Chris Evans <evanschris@users.noreply.github.com>
2019-06-27 13:49:00 +0100

    add info for patterns into add a service page

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,3 +1,15 @@
-$(function() {
+$(document).on('turbolinks:load', function() {
   $('.accordion').each(function() { $(this).accordion() });
+
+
+  if($('#pattern_confirmation').length) {
+    $('#pattern_checkboxes input').change(function() {
+      var pattern = $(this).val();
+      if($(this).is(":checked")) {
+        $('.pattern_text_'+pattern).slideDown();
+      } else {
+        $('.pattern_text_'+pattern).slideUp();
+      }
+    })
+  }
 })
