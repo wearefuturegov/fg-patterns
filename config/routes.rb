95bbc0198293188ba0490f1657f63eb4678e0d25
userman123 <dominic.freeman123@gmail.com>
2019-07-03 11:31:52 +0100

    Fix admin route bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
   resources :services, only: [:new, :index, :create, :show]
 
+  namespace :admin do
+    root to: "services#index"
+    resources :services, only: [:index, :edit, :update, :destroy]
+  end
+
   resources :patterns, only: [], param: :slug, path: '/', controller: 'services' do
     member do
       get :filter_by, to: 'services#index', path: '/'
     end
-  end
-
-  namespace :admin do
-    root to: "services#index"
-    resources :services, only: [:index, :edit, :update, :destroy]
   end
 
   root :to => "home#index"
